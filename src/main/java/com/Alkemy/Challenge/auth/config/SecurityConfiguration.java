28a5559bbd13856a39ddf375d96991a12476af94
Ignacio <joseignacio439@gmail.com>
2022-05-22 16:04:32 -0300

    SecurityConfiguration added.

--- src/main/java/com/Alkemy/Challenge/auth/config/SecurityConfiguration.java
+++ src/main/java/com/Alkemy/Challenge/auth/config/SecurityConfiguration.java
@@ -1,8 +1,54 @@
 package com.Alkemy.Challenge.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.Alkemy.Challenge.auth.filter.JwtRequestFilter;
+import com.Alkemy.Challenge.auth.service.UserDetailCustomService;
 
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserDetailCustomService userDetailCustomService;
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailCustomService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity.csrf().disable()
+		.authorizeHttpRequests().antMatchers("/auth/*").permitAll()
+		.anyRequest().authenticated()
+		.and().exceptionHandling()
+		.and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
 }
